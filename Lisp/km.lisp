da06722d8cd9b338eccf2f58879303a5b905128f
Davide Casella <casella.dvd@gmail.com>
2016-08-24 21:40:48 +0200

    Update km

--- Lisp/km.lisp
+++ Lisp/km.lisp
@@ -4,22 +4,23 @@
 ;;; Funzioni principali
 
 (defun km (observations k)
-	())
+  ())
 
 (defun centroid (observations)
-	())
+  (mapcar #'(lambda (coord) (/ coord (length observations)))
+          (reduce #'vsum observations)))
 
 (defun vsum (vector1 vector2)
-	())
+  (mapcar #'+ vector1 vector2))
 
 (defun vsub (vector1 vector2)
-	())
+  (mapcar #'- vector1 vector2))
 
 (defun innerprod (vector1 vector2)
-	())
+  (reduce #'+ (mapcar #'* vector1 vector2)))
 
 (defun norm (vector)
-	())
+  (sqrt (innerprod vector vector)))
 
 ;;; Funzioni ausiliarie
 
