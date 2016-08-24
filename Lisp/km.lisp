29401c83b84e464f283dc85c8749bc3b7aac48ce
Davide Casella <casella.dvd@gmail.com>
2016-08-24 17:17:21 +0200

    Update km

---
+++ Lisp/km.lisp
@@ -0,0 +1,25 @@
+;;;; Casella Davide 793631
+;;;; Nicolini Fabio 794467
+
+;;; Funzioni principali
+
+(defun km (observations k)
+	())
+
+(defun centroid (observations)
+	())
+
+(defun vsum (vector1 vector2)
+	())
+
+(defun vsub (vector1 vector2)
+	())
+
+(defun innerprod (vector1 vector2)
+	())
+
+(defun norm (vector)
+	())
+
+;;; Funzioni ausiliarie
+
---
+++ Prolog/km.pl
@@ -0,0 +1,21 @@
+% Casella Davide 793631
+% Nicolini Fabio 794467
+
+% Predicati principali
+
+km(_Observations, _K, _Clusters).
+
+centroid(_Observations, _Centroid).
+
+vsum(_Vector1, _Vector2, _V).
+
+vsub(_Vector1, _Vector2, _V).
+
+innerprod(_Vector1, _Vector2, _R).
+
+norm(_Vector, _N).
+
+new_vector(_Name, _Vector).
+
+% Predicati ausiliari
+
