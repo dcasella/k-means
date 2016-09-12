f9f62d2c887cec765dab1746ab46d65f76a4ac32
Davide Casella <casella.dvd@gmail.com>
2016-09-12 20:24:54 +0200

    Update Prolog - 3D testing

--- Prolog/km_test.pl
+++ Prolog/km_test.pl
@@ -1,2 +1,2 @@
 assert((make_obs(0, []) :- !)).
-assert((make_obs(N, [[X, Y] | Result]) :- random_between(-2, 2, X), random_between(-2, 2, Y), M is N-1, make_obs(M, Result))).
+assert((make_obs(N, [[X, Y, Z] | Result]) :- random_between(-50, 50, X), random_between(-50, 50, Y), random_between(-50, 50, Z), M is N-1, make_obs(M, Result))).
