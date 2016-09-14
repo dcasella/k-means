954e75c3758c8a2e07b11d0da7a61c4062e6cf51
Davide Casella <casella.dvd@gmail.com>
2016-09-14 19:13:15 +0200

    Update C - Normalized pointer declarations

--- C/src/km.c
+++ C/src/km.c
@@ -8,23 +8,23 @@
 #define ERR_NO_MEM -2
 
 // Declaration
-void print_vector(double* vector, int vector_size);
-void print_observations(double** observations, int observations_size, int vector_size);
-void print_clusters(int* clusters, double** observations, int k, int observations_size, int vector_size);
-int compare_clusters(int* cluster1, int* cluster2, int clusters_size);
+void print_vector(double *vector, int vector_size);
+void print_observations(double **observations, int observations_size, int vector_size);
+void print_clusters(int *clusters, double **observations, int k, int observations_size, int vector_size);
+int compare_clusters(int *cluster1, int *cluster2, int clusters_size);
 
-int *km(double** observations, int k, int observations_size, int vector_size);
-double *centroid(double** observations, int observations_size, int vector_size);
-double *vsum(double* vector1, double* vector2, int vector_size);
-double *vsub(double* vector1, double* vector2, int vector_size);
-double innerprod(double* vector1, double* vector2, int vector_size);
-double norm(double* vector, int vector_size);
+int *km(double **observations, int k, int observations_size, int vector_size);
+double *centroid(double **observations, int observations_size, int vector_size);
+double *vsum(double *vector1, double *vector2, int vector_size);
+double *vsub(double *vector1, double *vector2, int vector_size);
+double innerprod(double *vector1, double *vector2, int vector_size);
+double norm(double *vector, int vector_size);
 
 int rand_num(int size);
 
-double **initialize(double** observations, int k, int observations_size, int vector_size);
-int *partition(double** observations, double** cs, int k, int observations_size, int vector_size);
-double **re_centroids(int* clisters, double** observations, int k, int observations_size, int vector_size);
+double **initialize(double **observations, int k, int observations_size, int vector_size);
+int *partition(double **observations, double **cs, int k, int observations_size, int vector_size);
+double **re_centroids(int *clisters, double **observations, int k, int observations_size, int vector_size);
 
 // Implementation
 void print_vector(double *vector, int vector_size) {
