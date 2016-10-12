87b7dccb209c8bcdfac7276b12cce22ef1b2dbbe
Davide Casella <casella.dvd@gmailcom>
2016-10-12 14:29:44 +0200

    Create Ruby - Initial commit

---
+++ Ruby/km_test.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+#encoding utf-8
+
+require_relative 'km'
+
+if ARGV.length > 1
+	file_obj = File.new(ARGV[0], "r")
+	observations = []
+	k = ARGV[1].to_i
+
+	file_obj.each_line do |line|
+		observations[file_obj.lineno - 1] = line.split("\t").map(&:to_f)
+	end
+
+	print km(observations, k), "\n"
+
+	file_obj.close
+end
