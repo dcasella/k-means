dba4597ca5bdf42d9ddb8567c135e25486e89f9a
Davide Casella <casella.dvd@gmail.com>
2016-11-07 00:19:06 +0100

    Normalized whitespaces

--- C/main.c
+++ C/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "km.h"
+#include "include/km.h"
+
 
 // Example main() with file importing and k-means execution
 
@@ -16,11 +17,11 @@ int main(int argc, char *argv[]) {
 		int k = atoi(argv[4]);
 		double **observations;
 		double ***clusters;
-
+		
 		observations = (double **) malloc(sizeof(double *) * observations_size);
 		for (int i = 0; i < observations_size; i++)
 			observations[i] = (double *) malloc(sizeof(double) * vector_size);
-
+		
 		if ((fp = fopen(filename, "r+")) == NULL) {
 			printf("No such file or directory\n");
 			for (int i=0 ; i<observations_size ; ++i)
@@ -28,30 +29,30 @@ int main(int argc, char *argv[]) {
 			free(observations);
 			exit(1);
 		}
-
+		
 		for (int i = 0; i < observations_size; i++) {
 			for (int j = 0; j < vector_size; j++)
 				fscanf(fp, "%lf", &observations[i][j]);
 		}
-
+		
 		printf("Observations:\n");
 		print_observations(observations, observations_size, vector_size);
 		printf("\n\n");
-
+		
 		clusters = km(observations, k, observations_size, vector_size);
 		printf("Clusters:\n");
 		print_clusters(clusters, k, observations_size, vector_size);
 		printf("\n");
-			
+		
 		for (int i=0 ; i<k ; ++i)
 			free(clusters[i]);
 		free(clusters);
-
+		
 		for (int i=0 ; i<observations_size ; ++i) 
 			free(observations[i]);
 		free(observations);
 		fclose(fp);
 	}
-
+	
 	return 0;
 }
